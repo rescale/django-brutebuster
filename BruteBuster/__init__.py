340f64b1648e6b7f331c60ff81c71ef9f44e37b4
CalloRico <kaneshiro.ryan@gmail.com>
2013-07-06 18:55:54 -0700

    Bumping django-brutebuster version.

--- BruteBuster/__init__.py
+++ BruteBuster/__init__.py
@@ -4,7 +4,7 @@ Module preventing Brute Force attacks against
 django.contrib.auth.authenticate()
 """
 
-version = '0.1.9'
+version = '0.2.0'
 
 # from django.contrib import auth
 # from BruteBuster.decorators import protect_and_serve
