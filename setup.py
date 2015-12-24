8a613a56ca0f6f6ae6a788f76b820e2efd20f42a
Ryan Kaneshiro <kaneshiro.ryan@gmail.com>
2015-12-23 17:06:00 -0800

    Update to version 0.2.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(\
     name = "django-brutebuster",
-    version = "0.2.2",
+    version = "0.2.3",
     packages = find_packages(),
     author = "Cyber Security Consulting Ltd.",
     author_email = "contact@csc.bg",
